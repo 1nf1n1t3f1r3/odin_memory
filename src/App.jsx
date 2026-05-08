b72b48cb28e07970e678316f7d63f10827800854
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-05-08 15:47:50 +0200

    Added click handler Added score counter
    Core logic done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [clickedIds, setClickedIds] = useState([]);
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -27,15 +30,57 @@ function App() {
     fetchPokemon();
   }, []);
 
+  // Creates a shuffled copy of the array
+  const shuffleCards = (cards) => {
+    return [...cards].sort(() => Math.random() - 0.5);
+  };
+
+  // Shuffle and Score
+  const handleCardClick = (id) => {
+    setPokemonList(shuffleCards(pokemonList));
+
+    if (clickedIds.includes(id)) {
+      setScore(0);
+      setClickedIds([]);
+    } else {
+      const newScore = score + 1;
+      setScore(newScore);
+      setClickedIds([...clickedIds, id]);
+
+      // Update High Score if current score beats it
+      if (newScore > highScore) {
+        setHighScore(newScore);
+      }
+      //   if newScore == 12... Win
+    }
+  };
+
   return (
-    <div className="card-container">
-      {pokemonList.map((pokemon) => (
-        <div key={pokemon.id} className="card">
-          <img src={pokemon.image} alt={pokemon.name} />
-          <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
+    <div className="App">
+      <header className="header">
+        <h1>Pokemon Memory Game</h1>
+        <div className="scoreboard">
+          <p>Current Score: {score}</p>
+          <p>Best Score: {highScore}</p>
         </div>
-      ))}
+      </header>
+
+      <div className="card-container">
+        {pokemonList.map((pokemon) => (
+          <div
+            key={pokemon.id}
+            className="card"
+            onClick={() => handleCardClick(pokemon.id)}
+          >
+            <img src={pokemon.image} alt={pokemon.name} />
+            <p>
+              {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
 export default App;
